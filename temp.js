b76969bd8ec0e5139c132310ac76d972a19efc96
An9k <an9000@gmail.com>
2018-02-20 11:34:09 -0800

    Test files for Bitgo

---
+++ temp.js
@@ -0,0 +1,13 @@
+{
+    "address": "2My3Pnify5Hc1PMR6N8176h7izqhBESNS9y",
+    "chain": 10,
+    "index": 4,
+    "path": "/10/4",
+    "redeemScript": "00206552e6c84019cafb1f3cfd4bde016a821bcb80e545597575d1fd8b258caefbac",
+    "witnessScript": "52210347195d7156ab9402d57be40b10e4b7d016c16bf7858aed40630cce62017a716c2103509cd7bf72f3be4f7f34c85d3672f9026889ac7f116142abd9dcf31ca551fc0e210236a78f18f428ff8e7a0acf79ee836ff539f9397780d0c5eb3b332f06d92d299a53ae",
+    "wallet": "2NBfVBYdSKMkEWdXy5tghKxNNbKPPN8qtXd",
+    "sent": 0,
+    "received": 100000000,
+    "txCount": 1,
+    "balance": 100000000
+}
